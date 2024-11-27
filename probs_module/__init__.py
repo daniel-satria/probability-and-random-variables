8602fd53f69e4a75d3e8cb8d0bfc633baa9121a8
Daniel Satria <danielsatria31@gmail.com>
2024-11-27 14:30:20 +0000

    Add Discret Random Variable 2

--- probs_module/__init__.py
+++ probs_module/__init__.py
@@ -2,3 +2,4 @@ from . import module_python_proba_4
 from . import module_python_bayes_5
 from . import module_python_ind_probs_6
 from . import module_python_dis_random_variable_9
+from . import module_proba_10
