f0eab8d0d0f5c5562208070e0e82fa107a98d83f
Daniel Satria <danielsatria31@gmail.com>
2024-12-10 07:27:19 +0000

    Add Continuous Random Variable

--- probs_module/__init__.py
+++ probs_module/__init__.py
@@ -4,3 +4,4 @@ from . import module_python_ind_probs_6
 from . import module_python_dis_random_variable_9
 from . import module_proba_10
 from . import module_proba_11
+from . import module_proba_12
